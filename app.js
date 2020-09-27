786754623aecb2c3de9b0b4a0929a0a307556e07
Andrea Wu <lixianhyun@gmail.com>
2020-09-26 18:50:00 -0700

    add 'reviews' route and templating view for each movie dynamically

--- app.js
+++ app.js
@@ -3,40 +3,52 @@ const express = require("express"),
       request = require("request");
 
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public")); 
+
+const apiKey = "b4bf14820185df41ad677a7daef76b12";
 
 app.get("/", function(req, res){
-    const url = "https://api.themoviedb.org/3/movie/upcoming?api_key=b4bf14820185df41ad677a7daef76b12&language=en-US&page=1"
+    const url = "https://api.themoviedb.org/3/movie/upcoming?api_key=" + apiKey + "&language=en-US&page=1"
     
     request(url, function(error, response, body){
         if(!error && response.statusCode == 200){
         const data = JSON.parse(body);
-        res.render("main", {data: data});
+        res.render("main", {data});
         };
     });
 });
 
 app.get("/results", function(req, res){
-    const query = req.query.search;
-	const url = "https://api.themoviedb.org/3/search/movie?api_key=b4bf14820185df41ad677a7daef76b12&language=en-US&query=" + query
-	
-	request(url, function(error, response, body){
-		if(!error && response.statusCode == 200){
-			const data = JSON.parse(body);
-            res.render("results", {data: data});
-		};
-	});
-})
+    const query = req.query.search.toLowerCase().trim();
+	const url = "https://api.themoviedb.org/3/search/movie?api_key=" + apiKey + "&language=en-US&query=" + query;
+    
+    if(query !== ""){
+        request(url, function(error, response, body){
+            if(!error && response.statusCode == 200){
+                const data = JSON.parse(body);
+                res.render("results", {data});
+            };
+        });
+    } else {
+        res.redirect("/");
+    }
+});
 
-app.get("/reviews/:id", function(req, res){
-    const movieId = req.params.id
-    const url = "https://api.themoviedb.org/3/movie/" + movieId + "/reviews?api_key=b4bf14820185df41ad677a7daef76b12"
+app.get("/reviews/:title/:id/", function(req, res){
+    const movieId = req.params.id;
+    const movieTitle = req.params.title;
+    const url = "https://api.themoviedb.org/3/movie/" + movieId + "/reviews?api_key=" + apiKey;
 
     request(url, function(error, response, body){
         if(!error && response.statusCode == 200){
             const info = JSON.parse(body);
-            // console.log("this is from data: " + info + " end here!! ");
-            res.render("reviews", {info: info});
+            // console.log(info.total_results);
+            if(info.total_results !== 0){
+                res.render("reviews", {info, movieTitle});
+            }
+            else {
+                res.render("noneReviews", {movieTitle});
+            }
         }
     });
 });
